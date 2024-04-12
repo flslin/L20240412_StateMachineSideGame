688cfbda87526024ea639f78f68de08fbc5bc467
flslin <84002788+flslin@users.noreply.github.com>
2024-04-12 14:27:41 +0900

    애니메이션 스테이트머신으로 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    #region 애니메이터
+    public Animator anim { get; private set; } // 프로퍼티
+
+    #endregion
+
+    #region States
     public PlayerStateMachine stateMachine { get; private set; }
     public PlayerIdleState idleState { get; private set; }
     public PlayerMoveState moveState { get; private set; }
+    #endregion
 
     public void Awake()
     {
         stateMachine = new PlayerStateMachine();
 
-        idleState = new PlayerIdleState(this, stateMachine, "Idle");
-        moveState = new PlayerMoveState(this, stateMachine, "Move");
+        idleState = new PlayerIdleState(this, stateMachine, "idle");
+        moveState = new PlayerMoveState(this, stateMachine, "move");
     }
 
     private void Start()
     {
+        anim = GetComponentInChildren<Animator>();
+
         stateMachine.Initialize(idleState);
     }
 
--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -18,7 +18,7 @@ public class PlayerState
 
     public virtual void Enter()
     {
-        Debug.Log("ø£≈Õ " + animBoolName);
+        player.anim.SetBool(animBoolName, true);
     }
 
     public virtual void Update()
@@ -28,6 +28,6 @@ public class PlayerState
 
     public virtual void Exit()
     {
-        Debug.Log("ø¢Ω√∆Æ " + animBoolName);
+        player.anim.SetBool(animBoolName, false);
     }
 }
